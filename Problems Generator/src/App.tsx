2cfc01ea5cc12fc21a5ada1f2fcf2d907c2de2af
Rayan <154925122+RayaneGuebre@users.noreply.github.com>
2025-08-08 21:08:43 +0000

    added input functions but latex doesn't render correcty

--- Problems Generator/src/App.tsx
+++ Problems Generator/src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css'
+import {useState} from 'react';
 import '@mantine/core/styles.css';
-import { MantineProvider, createTheme, Title , Input, NativeSelect } from '@mantine/core';
+import { MantineProvider, createTheme, Title , Input, NativeSelect, Button } from '@mantine/core';
 import type { MantineColorsTuple } from '@mantine/core';
+import 'katex/dist/katex.min.css';
+import { BlockMath } from 'react-katex';
+
+
+
+
 
 const myColor: MantineColorsTuple = [
   '#effde7',
@@ -16,6 +23,7 @@ const myColor: MantineColorsTuple = [
   '#3d920d'
 ];
 
+
 const theme = createTheme({
   colors: {
     myColor,
@@ -23,11 +31,46 @@ const theme = createTheme({
 });
 
 export default function App() {
+  const generateExercise = async () => {
+  const generatedOutput =  await fetch('https://ai.hackclub.com/chat/completions', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  // body: '{\n        "messages": [{"role": "user", "content": "Tell me a joke!"}],\n        "model": "openai/gpt-oss-120b"\n    }',
+  body: JSON.stringify({
+    'messages': [
+      {
+        'role': 'user',
+        'content': `Ur task is to generate an exercise for a student preparing for a test, the test is about ${subjectInput} and the student is ${gradeInput}, i want you to output just the exercise in a latex format`
+      }
+    ],
+    'model': 'openai/gpt-oss-120b'
+  })
+});
+
+const data = await generatedOutput.json();
+let output = data.choices?.[0].message?.content || '';
+setLatexOutput(output);
+  }
+
+
+  const [gradeInput, setGradeInput] = useState('');
+  const [subjectInput, setSubjectInput] = useState('');
+  const [latexOutput, setLatexOutput] = useState(''); 
+
+  const handleGradeChange = (e: React.ChangeEvent<HTMLSelectElement>) => setGradeInput(e.target.value);
+  const handleSubjectChange = (e: React.ChangeEvent<HTMLInputElement>) => setSubjectInput(e.target.value);
+
+
   return <MantineProvider theme={theme}>{
         <div>
           <Title>Problems Generator</Title>
-          <NativeSelect label="Grade" description="Which grade are you in?" radius="md" data={['9th grade', '10th grade', '11th grade', "Undergraduate", "Postgraduate"]} />
-     <Input size="md" radius="md" placeholder='What are you studying?'></Input>
+          <NativeSelect label="Grade" value={gradeInput} onChange={handleGradeChange} description="Which grade are you in?" radius="md" data={['9th grade', '10th grade', '11th grade', "Undergraduate", "Postgraduate"]} />
+     <Input size="md" radius="md" value={subjectInput} onChange={handleSubjectChange} placeholder='What are you studying?'></Input>
      </div>}
+     <Button onClick={generateExercise}> Generate</Button>
+     <BlockMath>{latexOutput}</BlockMath>,
+       
     </MantineProvider>;
 }
